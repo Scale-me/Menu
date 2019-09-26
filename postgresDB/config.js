57383aa205aee945d33c974e4569002abe16d243
Daniel Kang <danieldonghoonkang@gmail.com>
2019-09-25 17:27:46 -0700

    created a host key in my postgres config file

--- postgresDB/config.js
+++ postgresDB/config.js
@@ -1,6 +1,6 @@
   
 module.exports = {
-  user: 'postgres',
-  password: '8653',
+  host: 'ec2-13-59-145-65.us-east-2.compute.amazonaws.com',
+  user: 'ec2-user@ip-172-31-41-254',
   database: 'menu'
 };
