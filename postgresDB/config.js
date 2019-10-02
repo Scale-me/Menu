1d9414bccc1cd72692a229f701d8165749980cab
Daniel Kang <danieldonghoonkang@gmail.com>
2019-10-02 09:22:11 -0700

    refactored config to have correct ec2 instance url

--- postgresDB/config.js
+++ postgresDB/config.js
@@ -1,8 +1,8 @@
-  
+
 module.exports = {
-  host: 'ec2-13-59-145-65.us-east-2.compute.amazonaws.com',
+  host: 'ec2-18-220-50-204.us-east-2.compute.amazonaws.com',
   port: 5432,
   user: 'ec2-user',
   password: '8653',
-  database: 'menu'
+  database: 'menu',
 };
